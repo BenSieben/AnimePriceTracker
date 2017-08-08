12a7de03ba54f053470168d45aa7bf0570323dce
Ben <waripod@hotmail.com>
2017-08-08 16:44:10 -0700

    Started to implement a CrawlData class, which will hold all the crawl data for a website's products which are being tracked

--- src/b7/tools/tracking/CrawlData.java
+++ src/b7/tools/tracking/CrawlData.java
@@ -1,5 +1,7 @@
 package b7.tools.tracking;
 
+import java.util.Comparator;
+
 /**
  * Represents the collection of crawl data
  * for a single website
@@ -9,5 +11,14 @@ public class CrawlData {
     // Name of the data being crawled (ex: "Sentai Filmworks Crawler")
     String title;
 
-
+    // Comparator to use for the TreeMap that holds all the crawl data, so we sort in alphabetical order
+    //   while ignoring any case differences that might have caused products to be placed in unexpected places
+    public final static Comparator<String> caseInsensitiveComparator = new Comparator<String>() {
+        @Override
+        public int compare(String string1, String string2) {
+            String s1Lowercase = string1.toLowerCase();
+            String s2Lowercase = string2.toLowerCase();
+            return s1Lowercase.compareTo(s2Lowercase);
+        }
+    };
 }
