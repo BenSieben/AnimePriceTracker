481e7474e358667b47f0807ff8d3f89f7bbfc148
Ben <waripod@hotmail.com>
2017-08-11 11:55:42 -0700

    Added a new testProduct1 test to make sure that a basic merge of PriceDateInfo objects in a product works as expected

--- test/b7/tools/tracking/ProductTest.java
+++ test/b7/tools/tracking/ProductTest.java
@@ -15,7 +15,7 @@ public class ProductTest {
     // Test that we cannot externally modify the productPriceHistory by adding new elements manually
     @Test
     public void testProductToString() {
-        Product product = new Product("Sample Product", "example.com/products/sample-product");
+        Product product = new Product("Sample Product", "http://www.example.com/products/sample-product");
 
         PriceDateInfo samplePriceDateInfo = new PriceDateInfo("2017-08-08", "2017-08-09", 19.99);
         product.addNewPriceDateInfo(samplePriceDateInfo);
@@ -23,8 +23,27 @@ public class ProductTest {
 
         List<PriceDateInfo> productPriceHistory = product.getPriceHistory();
         productPriceHistory.add(new PriceDateInfo("2017-08-15", "2017-08-20", 19.99));
-        String  newProductString = product.toString();
+        String newProductString = product.toString();;
 
         assertEquals(oldProductString, newProductString);
+    }
+
+    @Test
+    public void testProduct1() {
+        // Make a product and test a basic PriceDateInfo merge
+        Product product = new Product("Sample Product", "http://www.example.com/products/sample-product");
+
+        PriceDateInfo samplePriceDateInfo = new PriceDateInfo("2017-08-08", "2017-08-09", 19.99);
+        product.addNewPriceDateInfo(samplePriceDateInfo);
+
+        samplePriceDateInfo = new PriceDateInfo("2017-09-08", "2017-09-09", 19.99);
+        product.addNewPriceDateInfo(samplePriceDateInfo);
+
+        // Make a product which has the expected results from merge
+        Product expectedProduct = new Product("Sample Product", "http://www.example.com/products/sample-product");
+        expectedProduct.addNewPriceDateInfo(new PriceDateInfo("2017-08-08", "2017-09-09", 19.99));
+
+        // Check that the two products are equivalent
+        assertEquals(expectedProduct.toString(), product.toString());
     }
 }
