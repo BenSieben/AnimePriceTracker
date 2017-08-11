1221791c9d4b386817cb4003ab8fc188eda13bc9
Ben <waripod@hotmail.com>
2017-08-11 15:25:15 -0700

    Created getter and setter for title instance variable

--- src/b7/tools/tracking/CrawlData.java
+++ src/b7/tools/tracking/CrawlData.java
@@ -9,6 +9,8 @@ import java.util.TreeMap;
  */
 public class CrawlData {
 
+    public static final String DEFAULT_TITLE = "Crawl Data";
+
     // Comparator to use for the TreeMap that holds all the crawl data, so we sort in alphabetical order
     //   while ignoring any case differences that might have caused products to be placed in unexpected places
     public final static Comparator<String> CASE_INSENSITIVE_COMPARATOR = new Comparator<String>() {
@@ -20,7 +22,7 @@ public class CrawlData {
         }
     };
 
-    // Name of the data being crawled (ex: "Sentai Filmworks Crawler")
+    // Name of the data being crawled (ex: "Sentai Filmworks Crawl Date")
     private String title;
 
     // Structure to hold all the product information, with product title leading to Product details
@@ -31,7 +33,29 @@ public class CrawlData {
      * @param title the title of the CrawlData
      */
     public CrawlData(String title) {
-        this.title = title;
+        setTitle(title);
         productTreeMap = new TreeMap<String, Product>(CASE_INSENSITIVE_COMPARATOR);
+    }
+
+    /**
+     * Returns the title of the CrawlDate
+     * @return the title of the CrawlData
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Sets the title of the CrawlData to the newTitle
+     * @param newTitle the new title to give to the CrawlData
+     * @pre newTitle is not null and at least one character (otherwise title becomes DEFAULT_TITLE)
+     */
+    public void setTitle(String newTitle) {
+        if(newTitle == null || newTitle.length() == 0) {
+            this.title = DEFAULT_TITLE;
+        }
+        else {
+            this.title = newTitle;
+        }
     }
 }
