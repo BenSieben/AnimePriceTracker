4ba0364514dbf3fc1beb64eeda0f3c94ca2d06d3
Ben <waripod@hotmail.com>
2017-08-15 16:51:50 -0700

    Removed commented code in loadpage.js

--- scripts/loadpage.js
+++ scripts/loadpage.js
@@ -1,31 +1,3 @@
-/*// Saves image render of website to example.png
-var page = require('webpage').create();
-page.open('https://www.rightstufanime.com/category/Blu~ray,DVD?show=96', function(status) {
-  console.log("Status: " + status);
-  if(status === "success") {
-    page.render('example.png');
-  }
-  phantom.exit();
-});*/
-
-// From https://stackoverflow.com/questions/16856036/save-html-output-of-page-after-execution-of-the-pages-javascript
-// Should save HTML output of page after execution of JavaScript on the page
-// Does not save HTML after JavaScript (but before, where complains about no JavaScript)
-/*var page = new WebPage()
-var fs = require('fs');
-
-page.onLoadFinished = function() {
-  console.log("page load finished");
-  //page.render('export.png');
-  fs.write('example.html', page.content, 'w');
-  phantom.exit();
-};
-
-page.open("https://www.rightstufanime.com/category/Blu~ray,DVD?show=96", function() {
-  page.evaluate(function() {
-  });
-});*/
-
 // From https://stackoverflow.com/questions/9966826/save-and-render-a-webpage-with-phantomjs-and-node-js
 var page = require('webpage').create();
 var fs = require('fs');
