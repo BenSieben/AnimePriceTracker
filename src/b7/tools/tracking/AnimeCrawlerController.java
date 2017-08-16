e53fe374ef93a96cc693fc99c6f139b05e912294
Ben <waripod@hotmail.com>
2017-08-16 13:46:40 -0700

    Added a RightStufCrawler object as an instance variable in AnimeCrawlerController, as well as important methods for the RightStufCrawler (visit all pages, save, load)

--- src/b7/tools/tracking/AnimeCrawlerController.java
+++ src/b7/tools/tracking/AnimeCrawlerController.java
@@ -9,27 +9,38 @@ public class AnimeCrawlerController {
     // Constants for where we will place save data for any crawlers we use
     public static final String CRAWLERS_PATH = "savedata/crawlers/";  // Folder we save crawlers in
     public static final String SENTAI_FILMWORKS_CRAWLER_FILENAME = CRAWLERS_PATH + "sentaifilmworks.json";
+    public static final String RIGHT_STUF_CRAWLER_FILENAME = CRAWLERS_PATH + "rightstuf.json";
     public static final String CSVS_PATH = "savedata/csvs/";  // Folder we save CSVs from crawl data in
     public static final String SENTAI_FILMWORKS_CRAWLER_CSV_FILENAME = CSVS_PATH + PriceDateInfo.findCurrentDateString() + "_sentaifilmworks.csv";
+    public static final String RIGHT_STUF_CRAWLER_CSV_FILENAME = CSVS_PATH + PriceDateInfo.findCurrentDateString() + " rightstuf.csv";
 
     // The SentaiFilmworksCrawler being used by the controller
     private SentaiFilmworksCrawler sentaiFilmworksCrawler;
+
+    // The RightStufCrawler being used by the controller
+    private RightStufCrawler rightStufCrawler;
 
     /**
      * Constructs a new AnimeCrawlerController with no pre-loaded data
      */
     public AnimeCrawlerController() {
         sentaiFilmworksCrawler = new SentaiFilmworksCrawler();
+        rightStufCrawler = new RightStufCrawler();
     }
 
     /**
      * Constructs a new AnimeCrawler controller with pre-loaded data
      * @param sentaiFilmworksDataFilename filename for a previously-saved SentaiFilmworksCrawler object
+     * @param rightStufDataFilename filename for a previously-saved RightStufCrawler object
      */
-    public AnimeCrawlerController(String sentaiFilmworksDataFilename) {
+    public AnimeCrawlerController(String sentaiFilmworksDataFilename, String rightStufDataFilename) {
         sentaiFilmworksCrawler = loadSentaiFilmworksCrawler(sentaiFilmworksDataFilename);
         if(sentaiFilmworksCrawler == null) {
             sentaiFilmworksCrawler = new SentaiFilmworksCrawler();
+        }
+        rightStufCrawler = loadRightStufCrawler(rightStufDataFilename);
+        if(rightStufCrawler == null) {
+            rightStufCrawler = new RightStufCrawler();
         }
     }
 
@@ -43,11 +54,28 @@ public class AnimeCrawlerController {
     }
 
     /**
+     * Uses the Right Stuf Crawler to visit all pages (to get updated pricing information)
+     * @param printProgress true to print out found products to standard output, false to not print
+     * @return true if all pages were successfully visited, false otherwise
+     */
+    public boolean visitAllRightStufPages(boolean printProgress) {
+        return rightStufCrawler.visitAllPages(printProgress);
+    }
+
+    /**
      * Saves the SentaiFilmworksCrawler in this controller to the specified filename
      * @param filename the filename to use for the saved SentaiFilmworksCrawler
      */
     public void saveSentaiFilmworksCrawler(String filename) {
         CrawlerDataHandler.saveSentaiFilmworksCrawler(sentaiFilmworksCrawler, filename);
+    }
+
+    /**
+     * Saves the RightStufCrawler in this controller to the specified filename
+     * @param filename the filename to use for the saved RightStufCrawler
+     */
+    public void saveRightStufCrawler(String filename) {
+        CrawlerDataHandler.saveRightStufCrawler(rightStufCrawler, filename);
     }
 
     /**
@@ -57,6 +85,15 @@ public class AnimeCrawlerController {
      */
     private SentaiFilmworksCrawler loadSentaiFilmworksCrawler(String filename) {
         return CrawlerDataHandler.loadSentaiFilmworksCrawler(filename);
+    }
+
+    /**
+     * Loads a RightStufCrawler from the filename
+     * @param filename the file to load data from
+     * @return RightStufCrawler on successful load, null on failed load
+     */
+    private RightStufCrawler loadRightStufCrawler(String filename) {
+        return CrawlerDataHandler.loadRightStufCrawler(filename);
     }
 
     /**
