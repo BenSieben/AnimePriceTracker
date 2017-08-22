bfd3ad3a2cb6b33ce785aee5cc905cc4b697b13b
Ben <waripod@hotmail.com>
2017-08-21 20:09:21 -0700

    Started working on the basic operations panel and setting it up with the Anime Price Tracker GUI

---
+++ src/b7/tools/tracking/AnimePriceTrackerBasicOperationsPanel.java
@@ -0,0 +1,180 @@
+package b7.tools.tracking;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.ByteArrayOutputStream;
+
+/**
+ * JPanel that draws a basic Gui for performing basic commands
+ * that can also be performed from the command line interface
+ * in AnimePriceTracker
+ */
+public class AnimePriceTrackerBasicOperationsPanel extends JPanel {
+
+    // Constant for how often to update the text area
+    public static final int TEXT_AREA_REFRESH_PERIOD = 2000;
+
+    // Buttons on the panel to let users perform actions
+    /*
+    public static final int PARSE_BASE_SENTAI_FILMWORKS_PAGE = 1;
+    public static final int PARSE_BASE_RIGHT_STUF_PAGE = 2;
+    public static final int VISIT_ALL_SENTAI_FILMWORKS_PAGES = 3;
+    public static final int VISIT_ALL_RIGHT_STUF_PAGES = 4;
+    public static final int UPDATE_CRAWL_DATA = 5;
+    public static final int MAKE_CSVS = 6;
+     */
+    private JButton parseBaseSentaiFilmworksPageButton,
+            parseBaseRightStufPageButton,
+            visitAllSentaiFilmworksPagesButton,
+            visitAllRightStufPagesButton,
+            updateCrawlDataButton,
+            makeCsvsButton;
+
+    // Text area to let users see output
+    private JTextArea outputTextArea;
+
+    /**
+     * Constructs a new AnimePriceTrackerBasicOperationsPanel
+     * @param textAreaStream stream that is used to print messages into the output text area (set to
+     *                       null to manually update text via changeOutputTextAreaContents())
+     */
+    public AnimePriceTrackerBasicOperationsPanel(ByteArrayOutputStream textAreaStream) {
+        super();
+        setLayout(new GridLayout(0, 2));
+
+        // Create button panel to hold buttons
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BorderLayout());
+
+        // Label for button panel
+        JLabel buttonPanelLabel = new JLabel("Operations");
+
+        // Create a panel which holds all the buttons
+        JPanel buttonCollectionPanel = new JPanel();
+        buttonCollectionPanel.setLayout(new GridLayout(0, 1));
+
+        // Create all buttons for button collection panel
+        parseBaseSentaiFilmworksPageButton = new JButton("Parse base Sentai Filmworks page");
+        buttonCollectionPanel.add(parseBaseSentaiFilmworksPageButton);
+        parseBaseRightStufPageButton = new JButton("Parse base Right Stuf page");
+        buttonCollectionPanel.add(parseBaseRightStufPageButton);
+        visitAllSentaiFilmworksPagesButton = new JButton("Visit all Sentai Filmworks pages");
+        buttonCollectionPanel.add(visitAllSentaiFilmworksPagesButton);
+        visitAllRightStufPagesButton = new JButton("Visit all Right Stuf pages");
+        buttonCollectionPanel.add(visitAllRightStufPagesButton);
+        updateCrawlDataButton = new JButton("Update crawl data");
+        buttonCollectionPanel.add(updateCrawlDataButton);
+        makeCsvsButton = new JButton("Generate CSVs from crawl data");
+        buttonCollectionPanel.add(makeCsvsButton);
+
+        // Add label / button collection to button panel
+        buttonPanel.add(buttonPanelLabel, BorderLayout.NORTH);
+        buttonPanel.add(buttonCollectionPanel, BorderLayout.CENTER);
+
+        // Create text area panel for showing output
+        JPanel textAreaPanel = new JPanel();
+        textAreaPanel.setLayout(new BorderLayout());
+
+        // Create label for text area
+        JLabel textAreaLabel = new JLabel("Output");
+
+        // Set up the text area with a scroll pane attached to it
+        outputTextArea = new JTextArea();
+        outputTextArea.setEditable(false);  // Make it so the output text area cannot be edited by users
+        JScrollPane textAreaScrollPane = new JScrollPane(outputTextArea);
+
+        // Add label / text area to text area panel
+        textAreaPanel.add(textAreaLabel, BorderLayout.NORTH);
+        textAreaPanel.add(textAreaScrollPane, BorderLayout.CENTER);
+
+        // Add final button panel / text area panel to main panel
+        add(buttonPanel);
+        add(textAreaPanel);
+
+        // Update the outputTextArea every 2000 seconds with given argument stream (if text area stream is not null)
+        if(textAreaStream != null) {
+            final ByteArrayOutputStream textStream = textAreaStream;
+            Timer timer = new Timer(TEXT_AREA_REFRESH_PERIOD, new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    // Print out contents of textAreaStream
+                    System.out.println("Go");
+                    changeOutputTextAreaContents(textStream.toString());
+                }
+            });
+            timer.start();
+        }
+    }
+
+    /**
+     * Changes the outputTextArea's contents to the given new content
+     * @param newContent the text to show in the text area
+     */
+    protected void changeOutputTextAreaContents(String newContent) {
+        outputTextArea.setText(newContent);
+    }
+
+    /**
+     * Sets enabling of all components in the GUI to be set to the given argument
+     * (true = enable everything, false = disable everything)
+     * @param enable true to enable all components, false to disable all components
+     */
+    protected void setEnablingOfAllComponents(boolean enable) {
+        parseBaseSentaiFilmworksPageButton.setEnabled(enable);
+        parseBaseRightStufPageButton.setEnabled(enable);
+        visitAllSentaiFilmworksPagesButton.setEnabled(enable);
+        visitAllRightStufPagesButton.setEnabled(enable);
+        updateCrawlDataButton.setEnabled(enable);
+        makeCsvsButton.setEnabled(enable);
+    }
+
+    /**
+     * Adds new ActionListener for the parseBaseSentaiFilmworksPageButton JButton
+     * @param listener new ActionListener to add to the button
+     */
+    protected void addParseBaseSentaiFilmworksPageButtonActionListener(ActionListener listener) {
+        if(listener != null) parseBaseSentaiFilmworksPageButton.addActionListener(listener);
+    }
+
+    /**
+     * Adds new ActionListener for the parseBaseRightStufPageButton JButton
+     * @param listener new ActionListener to add to the button
+     */
+    protected void addParseBaseRightStufPageButtonActionListener(ActionListener listener) {
+        if(listener != null) parseBaseRightStufPageButton.addActionListener(listener);
+    }
+
+    /**
+     * Adds new ActionListener for the visitAllSentaiFilmworksPagesButton JButton
+     * @param listener new ActionListener to add to the button
+     */
+    protected void addVisitAllSentaiFilmworksPagesButtonActionListener(ActionListener listener) {
+        if(listener != null) visitAllSentaiFilmworksPagesButton.addActionListener(listener);
+    }
+
+    /**
+     * Adds new ActionListener for the visitAllRightStufPagesButton JButton
+     * @param listener new ActionListener to add to the button
+     */
+    protected void addVisitAllRightStufPagesButtonActionListener(ActionListener listener) {
+        if(listener != null) visitAllRightStufPagesButton.addActionListener(listener);
+    }
+
+    /**
+     * Adds new ActionListener for the updateCrawlDataButton JButton
+     * @param listener new ActionListener to add to the button
+     */
+    protected void addUpdateCrawlDataButtonActionListener(ActionListener listener) {
+        if(listener != null) updateCrawlDataButton.addActionListener(listener);
+    }
+
+    /**
+     * Adds new ActionListener for the makeCsvsButton JButton
+     * @param listener new ActionListener to add to the button
+     */
+    protected void addMakeCsvsButtonActionListener(ActionListener listener) {
+        if(listener != null) makeCsvsButton.addActionListener(listener);
+    }
+}
--- src/b7/tools/tracking/AnimePriceTrackerGUI.java
+++ src/b7/tools/tracking/AnimePriceTrackerGUI.java
@@ -2,8 +2,14 @@ package b7.tools.tracking;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
 
 /**
  * Class to make a GUI interface for the Anime
@@ -11,24 +17,172 @@ import java.awt.event.WindowListener;
  */
 public class AnimePriceTrackerGUI extends JFrame implements WindowListener {
 
+    // Reference to original System.out OutputStream / System.err OutputStream
+    private final OutputStream originalOutputStream = System.out;
+    private final OutputStream originalErrStream = System.err;
+
+    // The OutputStream we use to override the default System.out and System.err (and PrintStream that uses it)
+    private ByteArrayOutputStream frameOutputStream;
+    private PrintStream frameOutputStreamCapture;
+
     // Flag to keep track of when the window closes
     private volatile boolean hasClosed = false;
 
+    // Various swing components used in the GUI
+    private JTabbedPane tabPane;
+    private AnimePriceTrackerBasicOperationsPanel basicOperationsPanel;
+
+    /**
+     * Constructs a new AnimePriceTrackerGUI
+     */
     public AnimePriceTrackerGUI() {
         super("Anime Price Tracker");
 
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+        addWindowListener(this);
+
+        // Construct JPanels to construct the complete GUI in this JFrame
+
+        // Master JPanel that holds all other panels
+        JPanel masterPanel = new JPanel();
+        masterPanel.setLayout(new GridLayout(1, 1));
+
+        // Change System.out / System.err to be our own output stream so we can capture things written to System.out
+        frameOutputStream = new ByteArrayOutputStream();
+        frameOutputStreamCapture = new PrintStream(frameOutputStream);
+        System.setOut(frameOutputStreamCapture);
+        System.setErr(frameOutputStreamCapture);
+
+        frameOutputStreamCapture.println("Hi");
+        System.out.println(frameOutputStream.toString());
+
+        // Make tabbed pane to let users have tabs to switch between
+        tabPane = new JTabbedPane();
+
+        // TODO Create tab for basic CLI operations and add it to the tabPane
+        basicOperationsPanel = new AnimePriceTrackerBasicOperationsPanel(frameOutputStream);
+        tabPane.addTab("Basic Commands", null, basicOperationsPanel,
+                "Perform commands that can also be found on the command line interface");
+
+        // Add action listeners for the buttons on the basic operations panel
+        basicOperationsPanel.addParseBaseRightStufPageButtonActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO actually perform action
+                System.out.println("addParseBaseRightStufPageButtonActionListener");
+            }
+        });
+        basicOperationsPanel.addParseBaseRightStufPageButtonActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO actually perform action
+                System.out.println("addParseBaseRightStufPageButtonActionListener");
+            }
+        });
+        basicOperationsPanel.addVisitAllSentaiFilmworksPagesButtonActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO actually perform action
+                System.out.println("addVisitAllSentaiFilmworksPagesButtonActionListener");
+            }
+        });
+        basicOperationsPanel.addVisitAllRightStufPagesButtonActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO actually perform action
+                System.out.println("addVisitAllSentaiFilmworksPagesButtonActionListener");
+            }
+        });
+        basicOperationsPanel.addUpdateCrawlDataButtonActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO actually perform action
+                System.out.println("addUpdateCrawlDataButtonActionListener");
+            }
+        });
+        basicOperationsPanel.addMakeCsvsButtonActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO actually perform action
+                System.out.println("addMakeCsvsButtonActionListener");
+            }
+        });
+
+        // TODO Create tab for graphing price history of products
+        tabPane.addTab("Graph Product History", null, new JPanel(),
+                "Show graphs of product history");
+
+        // Making a pretend tab to add for testing things out
+        JPanel testButtonPanel = new JPanel();
+        final JButton testButton = new JButton("Press me!");
+        testButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Below command will freeze the GUI until it is completed
+                Thread thread = new Thread(){
+                    @Override
+                    public void run() {
+                        testButton.setEnabled(false);
+                        RightStufCrawler rightStufCrawler = new RightStufCrawler();
+                        try {
+                            sleep(5000);
+                        }
+                        catch(InterruptedException ex) {
+                            ex.printStackTrace();
+                        }
+                        rightStufCrawler.parseBasePage();
+                        SwingUtilities.invokeLater(new Runnable() {
+                            @Override
+                            public void run() {
+                                testButton.setText("Done!");
+                                testButton.setEnabled(true);
+                            }
+                        });
+                    }
+                };
+                thread.start();
+                // The below code causes GUI to freeze up until thread created just above finishes
+                /*SwingUtilities.invokeLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            thread.join();
+                        }
+                        catch(InterruptedException ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                });*/
+            }
+        });
+        testButtonPanel.add(testButton);
+
+        tabPane.addTab("Sample Tab", null, testButtonPanel,
+                "Sample tab which is for testing purposes");
+
+        // Add tabPane to this frame so it shows up
+        add(tabPane);
+
         setPreferredSize(new Dimension(1280, 720));
         setMinimumSize(new Dimension(640, 360));
         setLocationRelativeTo(null);
-
-        addWindowListener(this);
 
         requestFocus();
         pack();
         setVisible(true);
 
         hasClosed = false;
+    }
+
+    /**
+     * Sets enabling of all components in the GUI to be set to the given argument
+     * (true = enable everything, false = disable everything)
+     * @param enable true to enable all components, false to disable all components
+     */
+    private void setEnablingOfAllComponents(boolean enable) {
+        tabPane.setEnabled(enable);
+        basicOperationsPanel.setEnablingOfAllComponents(enable);
     }
 
     /**
@@ -42,12 +196,22 @@ public class AnimePriceTrackerGUI extends JFrame implements WindowListener {
     /**
      * Method that gets fired when the GUI window is closed
      * (sets hasClosed field to true so controlling programs
-     * can tell when window has closed)
+     * can tell when window has closed and resets the
+     * output / error streams of System back to their default streams)
      * @param e not used
      */
     @Override
     public void windowClosing(WindowEvent e) {
         hasClosed = true;
+        System.setOut(new PrintStream(originalOutputStream));
+        System.setErr(new PrintStream(originalErrStream));
+        frameOutputStreamCapture.close();
+        try {
+            frameOutputStream.close();
+        }
+        catch(IOException ex) {
+            // Do nothing if IOException occurs when trying to close stream
+        }
     }
 
     /**
