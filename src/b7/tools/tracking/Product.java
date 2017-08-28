cf98f7deb59aa179dd8ef9c53675bf289496f94b
Ben <waripod@hotmail.com>
2017-08-28 14:24:04 -0700

    Changed implementation of Product's addNewPriceDateInfo() to no longer do "extensions" of old PriceDateInfo object dates if there is a gap between the newest existing data and the new data to add. Instead, date gaps will be left as gaps in the data (i.e., not in the price history)

--- src/b7/tools/tracking/Product.java
+++ src/b7/tools/tracking/Product.java
@@ -76,7 +76,11 @@ public class Product {
 
         // Merge the newPriceDateInfo with the last entry in priceHistory
         PriceDateInfo latestPriceDateInfo = priceHistory.get(priceHistory.size() - 1);
-        List<PriceDateInfo> finalPriceDateInfos = mergePriceDateInfos(latestPriceDateInfo, newPriceDateInfo);
+
+        // New merging algorithm - PriceDateInfo dates will not be "extended" to cover dates of unknown data
+        List<PriceDateInfo> finalPriceDateInfos = mergePriceDateInfosWithoutExtension(latestPriceDateInfo, newPriceDateInfo);
+        // Old merging algorithm - PriceDateInfo dates might be "extended" to cover dates of unknown data
+        //List<PriceDateInfo> finalPriceDateInfos = mergePriceDateInfosWithExtension(latestPriceDateInfo, newPriceDateInfo);
 
         // Remove the latest entry from priceHistory, then add everything in finalPriceDateInfos
         priceHistory.remove(priceHistory.size() - 1);
@@ -84,12 +88,12 @@ public class Product {
     }
 
     /**
-     * Returns proper result of merging p1 and p2
+     * Returns proper result of merging p1 and p2 without "extending" p1's end date near p2's start date
      * @param p1 the "smaller" PriceDateInfo object to merge
      * @param p2 the "larger" PriceDateInfo object to merge
      * @return the PriceDateInfo merge results as a List (as there can be one or more objects as the merge result)
      */
-    private List<PriceDateInfo> mergePriceDateInfos(PriceDateInfo p1, PriceDateInfo p2) {
+    private List<PriceDateInfo> mergePriceDateInfosWithoutExtension(PriceDateInfo p1, PriceDateInfo p2) {
         List<PriceDateInfo> mergedPriceDateList = new ArrayList<PriceDateInfo>();
         if(p1.compareTo(p2) == 0) {  // If p1 and p2 are the same, we just return p1
             mergedPriceDateList.add(p1);
@@ -97,7 +101,85 @@ public class Product {
         }
 
         if(p1.compareTo(p2) > 0) {  // Call this method in reverse if user passed arguments in "swapped" order
-            return mergePriceDateInfos(p2, p1);
+            return mergePriceDateInfosWithoutExtension(p2, p1);
+        }
+
+        // Down here, we utilize the knowledge that p1.compareTo(p2) MUST be less than 0 (thanks to if-checks above)
+        // First compare start dates
+        int startDateComparison = p1.getStartDate().compareTo(p2.getStartDate());
+        if(startDateComparison == 0) {  // same start date
+            int endDateComparison = p1.getEndDate().compareTo(p2.getEndDate());
+            if(endDateComparison == 0) {  // p1 ended same as p2 (same start)
+                // p1 must cost less than p2 in this case (same dates and negative comparison), so just return p1
+                mergedPriceDateList.add(p1);
+                return mergedPriceDateList;
+            }
+            else if(endDateComparison < 0) {  // p1 ended before p2 (same start)
+                // p1 ends before p2 ends (same start), so we must compare prices to determine what to do here
+                double priceComparison = p1.getPrice() - p2.getPrice();
+                if(Math.abs(priceComparison) <= 0.00000000001) {  // p1 is essentially same cost as p2
+                    // set p1's end date to p2's end date and return that single element
+                    p1.setEndDate(p2.getEndDate());
+                    mergedPriceDateList.add(p1);
+                }
+                else {  // p1 is cheaper or more expensive than p2 (we do same thing in both situations)
+                    // set p2's start date to a day after p1's end date to resolve duplicate entries on same date(s)
+                    p2.setStartDate(DateTool.findDateOffset(p1.getEndDate(), 1));
+                    mergedPriceDateList.add(p1);
+                    mergedPriceDateList.add(p2);
+                }
+                return mergedPriceDateList;
+            }
+        }
+        else {  // p1 must have started before p2 (if it started after, p1.compareTo(p2) would not be negative)
+
+            // First compare the price (if price is same AND p1 ends just before p2 starts, we can "merge" the two)
+            double priceComparison = p1.getPrice() - p2.getPrice();
+            boolean p1EndsJustBeforeP2Starts = DateTool.findDateOffset(p1.getEndDate(), 1).equals(p2.getStartDate());
+            if(Math.abs(priceComparison) <= 0.00000000001 && p1EndsJustBeforeP2Starts) {  // p1 is essentially same cost as p2 and p1 ends right before p2 starts
+                String mergeStartDate = findMinStartDate(p1, p2);
+                String mergeEndDate = findMaxEndDate(p1, p2);
+                double mergePrice = p1.getPrice();
+                PriceDateInfo mergeInfo = new PriceDateInfo(mergeStartDate, mergeEndDate, mergePrice);
+                mergedPriceDateList.add(mergeInfo);
+                return mergedPriceDateList;
+            }
+
+            // Compare p1's end date with p2's start date to determine what the merge results will be
+            int p1EndVSP2Start = p1.getEndDate().compareTo(p2.getStartDate());
+            if(p1EndVSP2Start < 0) {  // p1 ends before p2 starts
+                // Just add the two elements in order
+                mergedPriceDateList.add(p1);
+                mergedPriceDateList.add(p2);
+                return mergedPriceDateList;
+            }
+            else { // p1 ends on the same day or after p2 starts
+                // Set back p1's end date to be behind one day before p2's start date and then return both elements
+                p1.setEndDate(DateTool.findDateOffset(p2.getStartDate(), -1));
+                mergedPriceDateList.add(p1);
+                mergedPriceDateList.add(p2);
+                return mergedPriceDateList;
+            }
+        }
+        return mergedPriceDateList;  // Technically impossible to get here, but needed to satisfy compiler
+    }
+
+    /**
+     * Returns proper result of merging p1 and p2 by "extending" p1's end date near p2's start date
+     * if there is a gap
+     * @param p1 the "smaller" PriceDateInfo object to merge
+     * @param p2 the "larger" PriceDateInfo object to merge
+     * @return the PriceDateInfo merge results as a List (as there can be one or more objects as the merge result)
+     */
+    private List<PriceDateInfo> mergePriceDateInfosWithExtension(PriceDateInfo p1, PriceDateInfo p2) {
+        List<PriceDateInfo> mergedPriceDateList = new ArrayList<PriceDateInfo>();
+        if(p1.compareTo(p2) == 0) {  // If p1 and p2 are the same, we just return p1
+            mergedPriceDateList.add(p1);
+            return mergedPriceDateList;
+        }
+
+        if(p1.compareTo(p2) > 0) {  // Call this method in reverse if user passed arguments in "swapped" order
+            return mergePriceDateInfosWithExtension(p2, p1);
         }
 
         // Down here, we utilize the knowledge that p1.compareTo(p2) MUST be less than 0 (thanks to if-checks above)
